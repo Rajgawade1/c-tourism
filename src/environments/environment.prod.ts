3df013a1da57342eec4a729647b1052d78f2a6b0
rajaramgawade <raj.gawade55@gmail.com>
2026-08-05 21:35:44 +0530

    Added chnages to load images from asset file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //cdnUrl:"https://d2ec66jwco4fye.cloudfront.net/",
   cdnUrl:"assets/",
-  backendUrl:"http://13.60.240.155:8080/",
+  backEndUrl:"http://13.60.240.155:8080/",
 };
